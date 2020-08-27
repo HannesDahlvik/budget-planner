e4b3fc4d133307c78bd409b87c7e115bbb91f37c
HannesDahl <hannes.dahlvik@gmail.com>
2020-08-27 14:06:45 +0300

    Added stuff to homepage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,40 +1,55 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import './Navbar.scss';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+const classes = (theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.primary.main
+    },
+    title: {
+        flexGrow: 1,
+        fontSize: 20
+    },
+});
 
-export default function ButtonAppBar() {
-  const classes = useStyles();
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+        window.addEventListener('scroll', () => {
+            const navbarWrapperElement = document.querySelector('.Navbar-root-1');
+            if (window.scrollY > 600) {
+                navbarWrapperElement.style.height = '64px';
+                navbarWrapperElement.classList.add('navbar-active');
+            } else {
+                navbarWrapperElement.style.height = '128px';
+                navbarWrapperElement.classList.remove('navbar-active');
+            }
+        });
+    }
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <div className={classes.root}>
+                <AppBar className="navbar navbar-transparent" position="static">
+                    <Toolbar>
+                        <Typography variant="overline" className={classes.title}>
+                            Budget planner
+                        </Typography>
+                        <Button href="/signup" color="inherit">Sign up</Button>
+                        <Button href="/login" color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
 }
+
+export default withStyles(classes)(Navbar);
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import './Homepage.scss';
+import Navbar from '../components/Navbar';
+import { Typography } from '@material-ui/core';
 
 export default class Homepage extends React.Component {
     render() {
         return (
-            <div>
-                Homepage
-            </div>
+            <>
+                <Navbar />
+                <div className="section">
+                    <div className="skewed"></div>
+                    <div className="big-ass-header">
+                        <Typography variant="h3">our mission is to big money</Typography>
+                    </div>
+                </div>
+
+                <div className="section">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos pariatur minima repellendus eaque illum accusantium porro magni veritatis alias odit illo provident earum, tenetur, quibusdam id magnam sunt consequuntur beatae minus soluta, nesciunt enim fugit. Tenetur reprehenderit illum, accusamus accusantium aperiam temporibus et quasi, recusandae necessitatibus mollitia velit alias dolore.</p>
+                </div>
+            </>
         )
     }
 }
