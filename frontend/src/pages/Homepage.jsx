5273d29147c2027fdd65a4d5eaf8bbaf1c6ce8e2
NiklasHastbacka <niklas.hastbacka@optistud.fi>
2020-08-28 13:56:41 +0300

    Update Homepage.jsx

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -4,6 +4,7 @@ export default class Homepage extends React.Component {
     render() {
         return (
             <div>
+                
                 Homepage
             </div>
         )
