10b568a7c7481cf7a1f834ce528cb68ddc23448e
NiklasHastbacka <niklas.hastbacka@optistud.fi>
2020-09-07 12:52:27 +0300

    made calendar

---
+++ frontend/src/pages/dasboard_pages/Calendar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Calendar, Views } from 'react-big-calendar'
+
+const ColoredDateCellWrapper = ({ children }) =>
+  React.cloneElement(React.Children.only(children), {
+    style: {
+      backgroundColor: 'lightblue',
+    },
+  })
+
+let Basic = ({ localizer }) => (
+  <Calendar
+    events={events}
+    views={allViews}
+    step={60}
+    showMultiDayTimes
+    max={dates.add(dates.endOf(new Date(2015, 17, 1), 'day'), -1, 'hours')}
+    defaultDate={new Date(2015, 3, 1)}
+    components={{
+      timeSlotWrapper: ColoredDateCellWrapper,
+    }}
+    localizer={localizer}
+  />
+)
+
+export default Basic
