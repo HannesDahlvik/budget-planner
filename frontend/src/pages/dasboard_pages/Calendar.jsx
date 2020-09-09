c69de301e3232a047c2f11a3b9b46055b0fd7b4c
NiklasHastbacka <niklas.hastbacka@optistud.fi>
2020-09-09 12:23:44 +0300

    working calendar

--- frontend/src/pages/dasboard_pages/Calendar.jsx
+++ frontend/src/pages/dasboard_pages/Calendar.jsx
@@ -4,6 +4,9 @@ import events from '../../utils/events'
 import * as dates from '../../utils/dates'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop'
+
+const DragAndDropCalendar = withDragAndDrop(Calendar)
 
 let allViews = Object
     .keys(Views)
@@ -15,18 +18,103 @@ const ColoredDateCellWrapper = ({children}) => React.cloneElement(React.Children
     }
 })
 
-const local = momentLocalizer(moment)
+class BigCalendar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            events: events,
+            displayDragItemInCell: true
+        }
 
-let Basic = () => (<Calendar
-    events={events}
-    views={allViews}
-    step={60}
-    showMultiDayTimes
-    max={dates.add(dates.endOf(new Date(), 'day'), -1, 'hours')}
-    defaultDate={new Date()}
-    components={{
-    timeSlotWrapper: ColoredDateCellWrapper
-}}
-    localizer={local}/>)
+        this.moveEvent = this
+            .moveEvent
+            .bind(this)
+        this.newEvent = this
+            .newEvent
+            .bind(this)
+    }
+    handleDragStart = event => {
+        this.setState({draggedEvent: event})
+    }
 
-export default Basic
+    dragFromOutsideItem = () => {
+        return this.state.draggedEvent
+    }
+
+    onDropFromOutside = ({start, end, allDay}) => {
+        const {draggedEvent} = this.state
+
+        const event = {
+            id: draggedEvent.id,
+            title: draggedEvent.title,
+            start,
+            end,
+            allDay: allDay
+        }
+
+        this.setState({draggedEvent: null})
+        this.moveEvent({event, start, end})
+    }
+
+    moveEvent = ({event, start, end, isAllDay: droppedOnAllDaySlot}) => {
+        const {events} = this.state
+
+        let allDay = event.allDay
+
+        if (!event.allDay && droppedOnAllDaySlot) {
+            allDay = true
+        } else if (event.allDay && !droppedOnAllDaySlot) {
+            allDay = false
+        }
+
+        const nextEvents = events.map(existingEvent => {
+            return existingEvent.id == event.id
+                ? {
+                    ...existingEvent,
+                    start,
+                    end
+                }
+                : existingEvent
+        })
+
+        this.setState({events: nextEvents})
+
+    }
+
+    resizeEvent = ({event, start, end}) => {
+        const {events} = this.state
+
+        const nextEvents = events.map(existingEvent => {
+            return existingEvent.id == event.id
+                ? {
+                    ...existingEvent,
+                    start,
+                    end
+                }
+                : existingEvent
+        })
+
+        this.setState({events: nextEvents})
+
+    }
+
+    newEvent(event) {}
+
+    render() {
+        const local = momentLocalizer(moment)
+        return (
+            <div>
+                <DragAndDropCalendar
+                    localizer={local}
+                    events={events}
+                    startAccessor="start"
+                    endAccessor="end"
+                    style={{
+                    height: 500
+                }}/>
+            </div>
+        )
+    }
+}
+
+export default BigCalendar
--- frontend/src/utils/events.js
+++ frontend/src/utils/events.js
@@ -1,5 +1,10 @@
 const now = new Date()
 
 export default [
-    
-  ]
+  {
+    id: 1,
+    title: 'Point in Time Event',
+    start: now,
+    end: now
+  }
+]
