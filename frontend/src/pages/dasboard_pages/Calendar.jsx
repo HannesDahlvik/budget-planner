dc810b020b7f114df21e7f8bd862157f42c49378
NiklasHastbacka <niklas.hastbacka@optistud.fi>
2020-09-10 10:10:06 +0300

    cleaner layout

--- frontend/src/pages/dasboard_pages/Calendar.jsx
+++ frontend/src/pages/dasboard_pages/Calendar.jsx
@@ -2,22 +2,33 @@ import React from 'react'
 import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
 import events from '../../utils/events'
 import * as dates from '../../utils/dates'
+import './Calendar.scss'
 import moment from 'moment'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const now = new Date()
 
 class BigCalendar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            events: []
+            events: [{
+                ammount: '',
+                title: '',
+                start: '',
+                end: '',
+                type: ''
+            }]
         }
     }
 
     componentDidMount() {
-        
+
     }
 
     render() {
+
+        
         const local = momentLocalizer(moment)
         return (
             <div>
