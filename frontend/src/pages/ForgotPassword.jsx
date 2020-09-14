d6454fbd4a52f96d382c261609d47b7f482c86a6
OliverSjolind <oliversjolind@gmail.com>
2020-09-14 10:42:54 +0300

    Revert "reverted bad merge"
    This reverts commit e90f7b459867b6592532afcb9b7c419e95790edf.

---
+++ frontend/src/pages/ForgotPassword.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+class ForgotPassword extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Forgot
+            </div>
+        )
+    }
+}
+
+export default ForgotPassword
